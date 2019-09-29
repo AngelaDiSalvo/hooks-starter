c552d190c32011f6e979c57df51c6fd67c57e67f
github username <github email address>
2019-09-29 10:23:06 -0500

    useContext hook

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import React, { useState, useEffect } from "react";
+import React, { useRef, createContext } from "react";
 import Toggle from "./Toggle";
-import useTitleInput from "./hooks/useTitleInput";
+import { useTitleInput } from "./hooks/useTitleInput";
+
+export const UserContext = createContext();
 
 const App = () => {
   // const [value, setValue] = useState(initialState);
   const [name, setName] = useTitleInput("");
+  const ref = useRef();
+
   return (
-    <div className="main-wrapper">
-      <h1>Level Up Dishes</h1>
-      <Toggle />
-      <form
-        onSubmit={e => {
-          e.preventDefault();
-          formSubmit(name, setName);
-        }}
-      >
-        <input
-          type="text"
-          onChange={e => setName(e.target.value)}
-          value={name}
-        />
-        <button>Submit</button>
-      </form>
-    </div>
+    <UserContext.Provider value={{ user: true }}>
+      <div className="main-wrapper" ref={ref}>
+        <h1 onClick={() => ref.current.classList.add("new-fkae-cls")}>
+          Level Up Dishes
+        </h1>
+        <Toggle />
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+            formSubmit(name, setName);
+          }}
+        >
+          <input
+            type="text"
+            onChange={e => setName(e.target.value)}
+            value={name}
+          />
+          <button>Submit</button>
+        </form>
+      </div>
+    </UserContext.Provider>
   );
 };
 
