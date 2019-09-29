e0e9b230ad91c1b138474ce19cec76c14a21ed11
github username <github email address>
2019-09-29 09:19:28 -0500

    useState useEffect custom hook

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Toggle from "./Toggle";
+import useTitleInput from "./hooks/useTitleInput";
 
 const App = () => {
   // const [value, setValue] = useState(initialState);
-  const [name, setName] = useState("");
-
+  const [name, setName] = useTitleInput("");
   return (
     <div className="main-wrapper">
       <h1>Level Up Dishes</h1>
       <Toggle />
-      <form onSubmit={ e => {
-        e.preventDefault();
-        formSubmit(name, setName);
-      }}>
-        <input 
-          type="text" 
-          onChange={e => setName(e.target.value)} 
-          value={name} 
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          formSubmit(name, setName);
+        }}
+      >
+        <input
+          type="text"
+          onChange={e => setName(e.target.value)}
+          value={name}
         />
         <button>Submit</button>
       </form>
@@ -25,8 +27,8 @@ const App = () => {
 };
 
 const formSubmit = (value, setValue) => {
-  console.log(value, "email sent")
-  setValue('')
-}
+  console.log(value, "email sent");
+  setValue("");
+};
 
 export default App;
